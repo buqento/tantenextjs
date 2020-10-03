42a3b2a99d9e13000b185051f5cfded74ea4e104
buqento <buqento@gmail.com>
2020-10-03 07:34:10 +0700

    update sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { DataKos } from '../utils/modals/fakeDb'
+import { DataArea } from '../utils/modals/fakeDb'
 
-const sitemapXml = data => {
+const sitemapXml = (dataKos, dataArea) => {
   let latestPost = 0;
-  let projectsXML = "";
+  let itemsXML = "";
+  let areasXML = "";
 
-  data.map(post => {
+  dataArea.map(area => {
+    const areaURL = `https://tantekos.com/area/${area.slug}`;
+    areasXML += `
+      <url>
+        <loc>${areaURL}</loc>
+      </url>`;
+  });
+
+  dataKos.map(post => {
     const postDate = post.date_modified;
     if (!latestPost || postDate > latestPost) {
       latestPost = postDate;
     }
-    const projectURL = `https://tantekos.com/${post.slug}`;
-    projectsXML += `
+    const itemURL = `https://tantekos.com/${post.slug}`;
+    itemsXML += `
       <url>
-        <loc>${projectURL}</loc>
+        <loc>${itemURL}</loc>
         <lastmod>${postDate}</lastmod>
         <priority>0.50</priority>
       </url>`;
@@ -23,17 +33,17 @@ const sitemapXml = data => {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>https://tantekos.com/</loc>
-        <lastmod>${latestPost}</lastmod>
         <priority>1.00</priority>
       </url>
-      ${projectsXML}
+      ${areasXML}
+      ${itemsXML}
     </urlset>`;
 };
 
 class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
     res.setHeader("Content-Type", "text/xml");
-    res.write(sitemapXml(DataKos));
+    res.write(sitemapXml(DataKos, DataArea));
     res.end();
   }
 }
