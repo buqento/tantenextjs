8cd273738374fb02679aa2738015e3f4dcab3622
buqento <buqento@gmail.com>
2020-10-13 06:42:57 +0700

    update metadata area

--- pages/area/[areaid].js
+++ pages/area/[areaid].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { string } from 'prop-types'
 import { Kost } from '../../utils/modals/Kost'
-import Firstupper from '../../utils/Firstupper'
+import { DtArea } from '../../utils/modals/Area'
 import HeadPage from '../../components/HeadPage'
 import ListKos from '../../components/ListKos'
 import NextHead from 'next/head'
@@ -14,9 +14,7 @@ class Detail extends React.Component {
     render() {
         const { slug } = this.props;
         const data = Kost.filter(item => item.location.title === slug)
-        let headTitle = "";
-        slug.split("-").map(index => headTitle += Firstupper(index) + " ")
-
+        const dataArea = DtArea.filter(item => item.slug === slug)
         const structureTypeBreadcrumbList =
             `{
           "@context": "https://schema.org",
@@ -43,17 +41,16 @@ class Detail extends React.Component {
                 "position": 3,
                 "item": {
                     "@id": "https://tantekos.com/area/${slug}",
-                    "name": "${headTitle}"
+                    "name": "${dataArea[0].title}"
                 }
             }
         ]
        }`
-
         const structureTypeItemList =
             `{
             "@context": "https://schema.org",
             "@type": "ItemList",
-            "name": "Area ${headTitle}",
+            "name": "Area ${dataArea[0].title}",
             "itemListElement": [
                 ${data.map((item, index) => `{
                     "@type": "ListItem",
@@ -62,21 +59,39 @@ class Detail extends React.Component {
                 }`)}
             ]
         }`
-
         const structureAreaPage = {
             '@graph': [
                 JSON.parse(structureTypeItemList),
                 JSON.parse(structureTypeBreadcrumbList)
             ]
         };
-
+        console.log(dataArea[0])
         return (
             <>
                 <NextHead>
+                    <title>Tantekos - Area {dataArea[0].title}</title>
+                    <meta name="googlebot" content="index, follow" />
+                    <meta name="robot" content="index, follow" />
+                    <meta name="application-name" content="Tantekos" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="title" content={`Kost Dan Kontrakan Murah Area ${dataArea[0].title}`} />
+                    <meta name="description" content={`Tersedia Kost Dan Kontrakan Murah Area ${dataArea[0].title}`} />
+                    <meta name="keywords" content={`tantekos, Info Kost, Cari kost, kost, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian, ${dataArea[0].title}`} />
+                    <meta property="og:title" content={`Kost Dan Kontrakan Murah Area ${dataArea[0].title}`} />
+                    <meta property="og:description" content={`Tersedia Kost Dan Kontrakan Murah Area ${dataArea[0].title}`} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content={`https://tantekos.com/area/${slug}`} />
+                    <meta property="og:image" content={dataArea[0].image} />
+                    <meta property="og:image:alt" content={dataArea[0].title} />
+                    <meta property="og:locale" content="id_ID" />
+                    <meta property="og:site_name" content="Tantekos" />
+                    <meta name="keyphrases" content="Info Kost, Cari Kost, Sewa Kost, Kost Bebas, Kost Murah, Kost pasutri, Aplikasi Kost, Aplikasi Pencarian Kost, Aplikasi Info Kost, APlikasi Cari Kost, Kost, Tantekost, Tantekosapp, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian" />
+                    <meta name="classification" content="Business, Rent House, Sewa Kost, Property, Rent Room, Info Kost, Information, Kost, Room, Cari Kost, Kost Murah, Kost Bebas, Application, Mobile Application, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian" />
+                    <link rel="canonical" content={`https://tantekos.com/area/${slug}`} />
                     <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structureAreaPage) }} />
                 </NextHead>
                 <div className="main-layout">
-                    <HeadPage title={`Area ${headTitle}`} />
+                    <HeadPage title={`Area ${dataArea[0].title}`} />
                     <ListKos data={data} />
                 </div>
             </>
