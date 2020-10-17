6e31e00669c85e47555745fd4df00a6ccefa5b13
buqento <buqento@gmail.com>
2020-10-17 08:06:06 +0700

    update homepage components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Kos from './kos'
 import Header from '../components/Header'
 import Area from '../components/Area'
 import Campaign from '../components/Campaign'
