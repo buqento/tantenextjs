d03882c725b637cb1815d4cf4f3c4cde02735b4b
buqento <buqento@gmail.com>
2020-10-19 07:14:51 +0700

    new post

--- components/AreaItem.js
+++ components/AreaItem.js
@@ -9,7 +9,7 @@ class AreaItem extends Component {
         return (
             <Link href={`area/${item.slug}`}>
                 <Card bg="light" style={{ width: '140px' }} className="mr-3">
-                    <Card.Img variant="bottom" src={`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${item.location.long},${item.location.lat},16/500x500?access_token=pk.eyJ1IjoiYnVxZW50byIsImEiOiJjanJ5a3p4cDkwZXJiNDlvYXMxcnhud3hhIn0.AhQ-vGYSIo6uTBmQD4MCsA`} />
+                    <Card.Img variant="bottom" src={`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${item.location.long},${item.location.lat},18/500x500?access_token=pk.eyJ1IjoiYnVxZW50byIsImEiOiJjanJ5a3p4cDkwZXJiNDlvYXMxcnhud3hhIn0.AhQ-vGYSIo6uTBmQD4MCsA`} />
                     <div className="pt-2 pb-2 text-center text-uppercase">{item.title}</div>
                 </Card>
             </Link>
