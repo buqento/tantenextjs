531c7eb6d02288cede3d96a05a967ab139884958
buqento <buqento@gmail.com>
2020-11-23 07:39:10 +0700

    add skeleton campaign

--- components/Campaign.js
+++ components/Campaign.js
@@ -8,7 +8,8 @@ class Campaign extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: null
+            data: null,
+            skeletonArr: [1, 2, 3]
         }
     }
     componentDidMount() {
@@ -23,30 +24,48 @@ class Campaign extends Component {
             })
     }
     render() {
-        const { data } = this.state;
+        const { data, skeletonArr } = this.state;
         return (
             <div className="my-3">
                 <DragScroll className="scroll-section flex">
-                    <div className="flex ml-3">
-                        {
-                            data && data
-                                .sort(
-                                    function compare(a, b) {
-                                        const dtModifiedA = a.date_modified;
-                                        const dtModifiedB = b.date_modified;
-                                        let comparison = 0;
-                                        if (dtModifiedA < dtModifiedB) {
-                                            comparison = 1;
-                                        } else if (dtModifiedA > dtModifiedB) {
-                                            comparison = -1;
-                                        }
-                                        return comparison;
-                                    }
-                                )
-                                .slice(0, 8)
-                                .map((item, index) => <CampaignItem key={index} item={item} />)
-                        }
-                    </div>
+                    {
+                        !data ?
+                            <div className="flex ml-3">
+                                {
+                                    skeletonArr.map((item, index) =>
+                                        <div key={index} className="rounded-xl overflow-hidden shadow-md mr-3 mb-2" style={{ width: '140px' }}>
+                                            <div className="animate-pulse w-full h-32 bg-gray-300" />
+                                            <div className="animate-pulse px-3 py-3 text-center">
+                                                <div className="px-2 my-1 w-full h-4 bg-gray-300" />
+                                                <div className="px-2 my-1 w-12 h-4 bg-gray-300" />
+                                                <div className="px-2 my-1 w-6 h-4 bg-gray-300" />
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                            :
+                            <div className="flex ml-3">
+                                {
+                                    data
+                                        .sort(
+                                            function compare(a, b) {
+                                                const dtModifiedA = a.date_modified;
+                                                const dtModifiedB = b.date_modified;
+                                                let comparison = 0;
+                                                if (dtModifiedA < dtModifiedB) {
+                                                    comparison = 1;
+                                                } else if (dtModifiedA > dtModifiedB) {
+                                                    comparison = -1;
+                                                }
+                                                return comparison;
+                                            }
+                                        )
+                                        .slice(0, 8)
+                                        .map((item, index) => <CampaignItem key={index} item={item} />)
+                                }
+                            </div>
+                    }
                 </DragScroll>
             </div>
         )
