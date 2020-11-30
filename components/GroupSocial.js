4122903e6155b66d216f7f754efe108a34788081
buqento <buqento@gmail.com>
2020-11-30 07:43:53 +0700

    update social media

--- components/GroupSocial.js
+++ components/GroupSocial.js
@@ -2,9 +2,42 @@ import React, { Component } from 'react'
 class Area extends Component {
     render() {
         return (
-            <div className="mt-3 mb-3">
-                <a href="https://www.facebook.com/groups/tantekos" target="blank"><div className="py-2 px-3">Informasi Rumah/Kos/Kontrakan Wilayah Maluku</div></a>
-                <a href="https://www.facebook.com/groups/811913479639737" target="blank"><div className="py-2 px-3">Informasi Rumah/Kos/Kontrakan Wilayah Papua</div></a>
+            <div className="mt-3 mb-3 divide-y-2 divide-gray-200">
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/485414032435519" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Jakarta
+                    </a>
+                </div>
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/221285929390212" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Yogyakarta
+                    </a>
+                </div>
+
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/762731311013431" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Jakarta
+                    </a>
+                </div>
+
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/485414032435519" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Bali
+                    </a>
+                </div>
+
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/tantekos" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Maluku
+                    </a>
+                </div>
+
+                <div className="py-2 px-3">
+                    <a href="https://www.facebook.com/groups/811913479639737" target="blank">
+                        Rumah/Kost/Kontrakan Wilayah Papua
+                    </a>
+                </div>
+
             </div>
         )
     }
