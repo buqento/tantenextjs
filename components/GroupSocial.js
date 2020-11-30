2d60da6192f5d61d9a13005ea34c3d29c4905fb1
buqento <buqento@gmail.com>
2020-11-30 07:52:57 +0700

    update social media

--- components/GroupSocial.js
+++ components/GroupSocial.js
@@ -2,39 +2,33 @@ import React, { Component } from 'react'
 class Area extends Component {
     render() {
         return (
-            <div className="mt-3 mb-3 divide-y-2 divide-gray-200">
+            <div className="my-3 mx-3 divide-y-2 divide-gray-150">
                 <div className="py-2 px-3">
                     <a href="https://www.facebook.com/groups/485414032435519" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Jakarta
+                        Rumah/Kost/Kontrakan DKI Jakarta
                     </a>
                 </div>
                 <div className="py-2 px-3">
                     <a href="https://www.facebook.com/groups/221285929390212" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Yogyakarta
+                        Rumah/Kost/Kontrakan Yogyakarta
                     </a>
                 </div>
 
                 <div className="py-2 px-3">
                     <a href="https://www.facebook.com/groups/762731311013431" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Jakarta
-                    </a>
-                </div>
-
-                <div className="py-2 px-3">
-                    <a href="https://www.facebook.com/groups/485414032435519" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Bali
+                        Rumah/Kost/Kontrakan Bali
                     </a>
                 </div>
 
                 <div className="py-2 px-3">
                     <a href="https://www.facebook.com/groups/tantekos" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Maluku
+                        Rumah/Kost/Kontrakan Maluku
                     </a>
                 </div>
 
                 <div className="py-2 px-3">
                     <a href="https://www.facebook.com/groups/811913479639737" target="blank">
-                        Rumah/Kost/Kontrakan Wilayah Papua
+                        Rumah/Kost/Kontrakan Papua
                     </a>
                 </div>
 
