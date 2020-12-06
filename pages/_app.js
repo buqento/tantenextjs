5b5d576ae54b93f8139625692daa7cf16f0a0536
buqento <buqento@gmail.com>
2020-12-06 20:00:15 +0700

    add loading global

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import Router from "next/router";
+import { shape, func } from 'prop-types'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'swiper/swiper.scss'
 import '../styles/globals.css'
 import '../styles/styles.css'
-import 'swiper/swiper.scss'
-import { shape, func } from 'prop-types'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => { setLoading(true) }
+    const end = () => { setLoading(false) }
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return (
+    <>
+      {
+        loading ? <div className="container-center text-center">Loading...</div>
+          : <Component {...pageProps} />
+      }
+    </>
+  );
+  // return <Component {...pageProps} />
 }
 
 MyApp.propTypes = {
