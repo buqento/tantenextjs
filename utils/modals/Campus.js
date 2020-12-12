7955de9f94bba3077617b938279d58d32c73e1d6
buqento <buqento@gmail.com>
2020-12-12 14:09:31 +0700

    add campus

--- utils/modals/Campus.js
+++ utils/modals/Campus.js
@@ -16,5 +16,6 @@ export const Campus = [
     { id: 15, slug: 'institut-transportasi-dan-logistik-trisakti', name: 'Institut Transportasi dan Logistik Trisakti', province: 'Jakarta Timur' },
     { id: 16, slug: 'universitas-kristen-indonesia', name: 'Universitas Kristen Indonesia', province: 'Jakarta Timur' },
     { id: 17, slug: 'universitas-muhammadiyah-yogyakarta', name: 'Universitas Muhammadiyah Yogyakarta', province: 'Yogyakarta' },
-    { id: 18, slug: 'universitas-pembangunan-nasional-veteran-yogyakarta', name: 'Universitas Pembangunan Nasional Veteran Yogyakarta', province: 'Yogyakarta' }
+    { id: 18, slug: 'universitas-pembangunan-nasional-veteran-yogyakarta', name: 'Universitas Pembangunan Nasional Veteran Yogyakarta', province: 'Yogyakarta' },
+    { id: 19, slug: 'institut-teknologi-yogyakarta', name: 'Institut Teknologi Yogyakarta', province: 'Yogyakarta' }
 ]
