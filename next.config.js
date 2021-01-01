f680d3bce688afc72719f9497e91a99fb28cd607
buqento <buqento@gmail.com>
2021-01-01 16:28:42 +0700

    set env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const Dotenv = require("dotenv-webpack");
 module.exports = {
     images: {
         domains: ['https://cdn.statically.io/img/i.imgur.com/'],
     },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(new Dotenv({ silent: true }));
+        return config;
+    }
 }
