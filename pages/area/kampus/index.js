6b2ed0a67d7ed70c8d2c920a7796b636e6670d8a
buqento <buqento@gmail.com>
2021-01-06 18:57:53 +0700

    add search

--- pages/area/kampus/index.js
+++ pages/area/kampus/index.js
@@ -1,40 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Router from 'next/router'
 import { Campus } from '../../../utils/modals/Campus'
-import HeadPage from '../../../components/HeadPage'
 import Generateslug from '../../../utils/Generateslug'
-import { BiChevronRight } from 'react-icons/bi'
-
-class AllCampus extends React.Component {
-    render() {
-        return (
-            <>
-                <div className="main-layout">
-                    <HeadPage title="Kost &amp; Kontrakan Dekat Kampus" />
-                    <div className="container divide-y divide-gray-400">
-                        {
-                            Campus
-                                .sort(function (a, b) {
-                                    var nameA = Generateslug(a.name.toUpperCase());
-                                    var nameB = Generateslug(b.name.toUpperCase());
-                                    if (nameA < nameB) return -1;
-                                    if (nameA > nameB) return 1;
-                                    return 0;
-                                })
-                                .map((item, index) =>
-                                    <div className="py-3 px-3" key={index}>
-                                        <a href={`../../area/kampus/${Generateslug(item.name)}`}>
-                                            <div className="d-flex">
-                                                <span className="w-full">{item.name}</span>
-                                                <span className="ml-0.5 float-right self-center"><BiChevronRight size={28} className="inline ml-1 mb-1" /></span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                )
-                        }
-                    </div>
-                </div>
-            </>
-        )
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
+const Filter = () => {
+    const [name, setName] = useState("")
+    const filterItems = (keyword) => {
+        let query = keyword.toLowerCase();
+        return Campus.filter(item => item.name.toLowerCase().indexOf(query) >= 0);
     }
+    const data = filterItems(name)
+    return (
+        <>
+            <div className="main-layout">
+
+                <div className="d-flex sticky top-0 bg-white border-bottom">
+                    <span onClick={() => Router.push('/')}>
+                        <BiChevronLeft size={40} className="inline pb-1 mt-3 ml-2" />
+                    </span>
+                    <input className="border rounded w-full py-3 px-3 my-2 mx-3 text-gray-700 leading-tight focus:outline-none" id="name" type="text" placeholder="Cari Kampus" value={name} onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+
+
+                <div className="container divide-y divide-gray-400">
+                    {
+                        data
+                            .sort(function (a, b) {
+                                var nameA = Generateslug(a.name.toUpperCase());
+                                var nameB = Generateslug(b.name.toUpperCase());
+                                if (nameA < nameB) return -1;
+                                if (nameA > nameB) return 1;
+                                return 0;
+                            })
+                            .map((item, index) =>
+                                <div className="py-3 px-3" key={index}>
+                                    <a href={`../../area/kampus/${Generateslug(item.name)}`}>
+                                        <div className="d-flex">
+                                            <span className="w-full">{item.name}</span>
+                                            <span className="ml-0.5 float-right self-center"><BiChevronRight size={28} className="inline ml-1 mb-1" /></span>
+                                        </div>
+                                    </a>
+                                </div>
+                            )
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
-export default AllCampus;
+export default Filter
