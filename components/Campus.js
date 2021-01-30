f2b086f3703e1a028ea9d15675cfde5787d6d3b9
buqento <buqento@gmail.com>
2021-01-30 19:25:45 +0700

    ui improve

--- components/Campus.js
+++ components/Campus.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Campus } from '../utils/modals/Campus'
 import Link from 'next/link'
 import Generateslug from '../utils/Generateslug'
-import Titlecase from '../utils/Titlecase'
 class ComponentCampus extends Component {
     render() {
         const TitleCase = (str, splitBy = ' ') => {
@@ -10,15 +9,15 @@ class ComponentCampus extends Component {
             return str[0].charAt(0) + str[1].charAt(0)
         }
         return (
-            <div className="container">
+            <div className="container divide-y">
                 {
                     Campus
                         .sort(() => .5 - Math.random())
                         .slice(0, 5)
                         .map((item, index) =>
                             <Link key={index} href={`area/kampus/${Generateslug(item.name)}`}>
-                                <div className="flex rounded-xl bg-gray-200 hover:bg-indigo-700 hover:text-white border py-2 px-2 my-2 align-middle text-gray-900 items-center ">
-                                    <div class="m-1 mr-2 px-3 relative flex justify-center items-center rounded-lg hover:bg-gray-500 bg-indigo-700 text-xl text-white uppercase" style={{ width: '50px', height: '50px' }}>{TitleCase(item.name)}</div>
+                                <div className="flex hover:text-indigo-700 py-1 align-middle text-gray-900 items-center cursor-pointer">
+                                    <div class="m-1 mr-2 px-3 relative flex justify-center items-center rounded-lg hover:bg-gray-500 bg-indigo-700 text-xl text-white uppercase" style={{ width: '40px', height: '40px' }}>{TitleCase(item.name)}</div>
                                     <span className="clamp-2 leading-tight">{item.name}</span>
                                 </div>
                             </Link>
--- components/GroupSocial.js
+++ components/GroupSocial.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react'
-import { BiChevronRight } from "react-icons/bi"
+import Link from 'next/link'
 class Area extends Component {
     render() {
         const group = [
             {
-                name: 'Rumah/Kost/Kontrakan DKI Jakarta',
-                url: 'https://www.facebook.com/groups/485414032435519'
+                name: 'Informasi Rumah, Kost, Kontrakan Daerah Khusus Ibukota Jakarta',
+                url: 'https://www.facebook.com/groups/485414032435519',
+                image: 'jakarta'
             },
             {
-                name: 'Rumah/Kost/Kontrakan Yogyakarta',
-                url: 'https://www.facebook.com/groups/221285929390212'
+                name: 'Informasi Rumah, Kost, Kontrakan Daerah Istimewa Yogyakarta',
+                url: 'https://www.facebook.com/groups/221285929390212',
+                image: 'yogyakarta'
             },
             {
-                name: 'Rumah/Kost/Kontrakan Bali',
-                url: 'https://www.facebook.com/groups/762731311013431'
+                name: 'Informasi Rumah, Kost, Kontrakan Daerah Bali',
+                url: 'https://www.facebook.com/groups/762731311013431',
+                image: 'bali'
             },
             {
-                name: 'Rumah/Kost/Kontrakan Maluku',
-                url: 'https://www.facebook.com/groups/tantekos'
+                name: 'Informasi Rumah, Kost, Kontrakan Daerah Maluku & Maluku Utara',
+                url: 'https://www.facebook.com/groups/tantekos',
+                image: 'maluku'
             },
             {
-                name: 'Rumah/Kost/Kontrakan Papua',
-                url: 'https://www.facebook.com/groups/811913479639737'
+                name: 'Informasi Rumah, Kost, Kontrakan Daerah Papua & Papua Barat',
+                url: 'https://www.facebook.com/groups/811913479639737',
+                image: 'papua'
             }
         ]
         return (
             <div className="my-3 mx-3 divide-y">
                 {
                     group.map((item, index) =>
-                        <div className="py-2" key={index}>
-                            <a href={item.url} target="blank">
-                                <span>{item.name}</span>
-                                <span className="float-right"><BiChevronRight size={28} className="inline ml-1 mb-1" /></span>
-                            </a>
-                        </div>
+                        <Link key={index} href={item.url}>
+                            <div className="flex hover:text-indigo-700 py-2 align-middle items-center cursor-pointer">
+                                <img src={`/static/images/group/${item.image}.jpg`} alt={item.title} className="float-left mr-2 rounded-lg" width={40} />
+                                <span className="clamp-2 leading-tight">{item.name}</span>
+                            </div>
+                        </Link>
                     )
                 }
             </div>
