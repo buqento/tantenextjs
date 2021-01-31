d2f6379cb1a0a36c05b81eac475826d4a359354c
buqento <buqento@gmail.com>
2021-01-31 09:16:25 +0700

    improve near campus

--- pages/area/kampus/index.js
+++ pages/area/kampus/index.js
@@ -3,6 +3,7 @@ import Router from 'next/router'
 import { Campus } from '../../../utils/modals/Campus'
 import { DtProvinsi } from '../../../utils/modals/Provinsi'
 import CampusList from '../../../components/CampusList'
+import Layout from '../../../components/Layout'
 import Generateslug from '../../../utils/Generateslug'
 import { BiChevronLeft } from 'react-icons/bi'
 const Filter = () => {
@@ -13,30 +14,25 @@ const Filter = () => {
     }
     const data = filterItems(name)
     return (
-        <>
-            <div className="main-layout">
-                <div className="d-flex sticky top-0 bg-white border-bottom">
-                    <span onClick={() => Router.push('/')}>
-                        <BiChevronLeft size={40} className="inline mb-3 mt-3 ml-2" />
-                    </span>
-                    <input className="w-full rounded-full px-4 my-3 mr-3 text-gray-700 leading-tight focus:outline-none font-medium border" id="name" type="text" placeholder="Masukan Nama Kampus" value={name} onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                {
-                    DtProvinsi
-                        .sort(function (a, b) {
-                            var nameA = Generateslug(a.title.toUpperCase());
-                            var nameB = Generateslug(b.title.toUpperCase());
-                            if (nameA < nameB) return -1;
-                            if (nameA > nameB) return 1;
-                            return 0;
-                        })
-                        .map(provinsi =>
-                            <CampusList name={name} locationProvince={provinsi.title} />
-                        )
-                }
+        <Layout>
+            <div className="d-flex sticky top-0 bg-white border-bottom">
+                <input className="w-full rounded-full px-4 py-3 my-3 mx-3 text-gray-700 leading-tight focus:outline-none font-medium border" id="name" type="text" placeholder="Masukan Nama Kampus" value={name} onChange={(e) => setName(e.target.value)}
+                />
             </div>
-        </>
+            {
+                DtProvinsi
+                    .sort(function (a, b) {
+                        var nameA = Generateslug(a.title.toUpperCase());
+                        var nameB = Generateslug(b.title.toUpperCase());
+                        if (nameA < nameB) return -1;
+                        if (nameA > nameB) return 1;
+                        return 0;
+                    })
+                    .map(provinsi =>
+                        <CampusList name={name} locationProvince={provinsi.title} />
+                    )
+            }
+        </Layout>
     )
 }
 export default Filter
