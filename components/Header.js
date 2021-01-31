178f661fda0f8089705ea390060997dcdc6ebb77
buqento <buqento@gmail.com>
2021-01-31 19:39:48 +0700

    seo improvement

--- components/Header.js
+++ components/Header.js
@@ -80,10 +80,14 @@ class Header extends Component {
                 structureTypeLocalBusiness
             ]
         };
+        const images = ['tt9t2IU', 'NaAULjD', 'RSbvRHn', 'DnxVdqt', 'kOuWQYi', 'TjA9SEq', 'stmSYZ2', 'yXRAu9W', 'rtX3zp9', 'i2aQSZ9'
+        ]
+        const rand = Math.floor(Math.random() * 10)
+        const title = 'Cari Kost Dan Kontrakan Harian Bulanan Tahunan Murah Terjangkau Nyaman Strategis'
         return (
             <>
                 <NextHead>
-                    <title>Cari Kost Dan Kontrakan Harian Bulanan Tahunan Murah Terjangkau Nyaman Strategis</title>
+                    <title>{title}</title>
                     <meta name="description" content="Tersedia Kost Dan Kontrakan Harian Bulanan Tahunan Murah Terjangkau Nyaman Strategis Di Sekitar Anda" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="googlebot" content="index, follow" />
@@ -92,6 +96,14 @@ class Header extends Component {
                     <meta name="application-name" content="Tantekos" />
                     <meta name="classification" content="Business, Rent House, Sewa Kost, Property, Rent Room, Info Kost, Information, Kost, Room, Cari Kost, Kost Murah, Kost Bebas, Application, Mobile Application, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian" />
                     <meta name="keywords" content="Tantekos, Info Kost, Cari Kost, Kost, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian" />
+                    <meta property="og:image" content={`https://cdn.statically.io/img/i.imgur.com/w=300/${images[rand]}.webp`} />
+                    <meta property="og:image:alt" content={title} />
+                    <meta property="og:image:width" content="300" />
+                    <meta property="og:image:height" content="300" />
+                    <meta property="og:locale" content="id_ID" />
+                    <meta property="og:site_name" content="Tantekos" />
+                    <meta property="fb:app_id" content="3234331779955939" />
+                    <meta name="keyphrases" content="Info Kost, Cari Kost, Sewa Kost, Kost Bebas, Kost Murah, Kost pasutri, Aplikasi Kost, Aplikasi Pencarian Kost, Aplikasi Info Kost, APlikasi Cari Kost, Kost, Tantekost, Tantekosapp, Kamar Kost, Kamar Kos, Kostan, Kos, Rumah Kost, Rumah Kos, Kost Harian" />
                     <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(structureHomePage) }} />
                 </NextHead>
             </>
