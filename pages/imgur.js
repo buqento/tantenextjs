6a2e8be0305e9748c5cd6401c61d7eff2a333165
buqento <buqento@gmail.com>
2021-04-25 13:12:32 +0700

    debug imgur

---
+++ pages/imgur.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class Imgur extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedFile: null
+        }
+    }
+
+    onFileChange = event => {
+        this.setState({ selectedFile: event.target.files[0] });
+    }
+
+    onFileUpload = () => {
+        const { selectedFile } = this.state
+        // Create an object of formData 
+        const formdata = new FormData();
+
+        // Update the formData object 
+        formdata.append("image", selectedFile);
+
+        // Request made to the backend api 
+        // Send formData object 
+        formdata.append("image", selectedFile)
+        fetch("https://api.imgur.com/3/image", {
+            method: "post",
+            headers: {
+                Authorization: "Client-ID e6aa071d345d18f"
+            },
+            body: formdata
+        }).then(data => data.json()).then(data => {
+            console.log(data)
+        })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="file" onChange={this.onFileChange} accept="image/*" />
+                <button onClick={this.onFileUpload}>Upload</button>
+            </div >
+        )
+    }
+}
+export default Imgur
+
+
+
+
+
