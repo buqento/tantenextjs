f2cda5e741d90e491ebdedff688262b6fbf8360e
buqento <buqento@gmail.com>
2021-05-02 21:28:05 +0700

    fix imgur

--- pages/imgur.js
+++ pages/imgur.js
@@ -1,55 +1,39 @@
 import React, { Component } from 'react'
-
 class Imgur extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            selectedFile: null,
-            files: []
+            selectedFile: null
         }
     }
 
     onFileChange = event => {
-        // this.setState({ selectedFile: event.target.files[0] });
-        this.setState({ files: [...this.state.files, ...event.target.files] })
+        this.setState({ selectedFile: event.target.files[0] });
     }
 
     onFileUpload = () => {
-        const { selectedFile, files } = this.state
-        // Create an object of formData 
+        const { selectedFile } = this.state
         const formdata = new FormData();
-        console.log(files);
-
-        // Update the formData object 
-        // formdata.append("image", selectedFile);
-
-        // Request made to the backend api 
-        // Send formData object 
-        // formdata.append("image", files)
-        // fetch("https://api.imgur.com/3/image", {
-        //     method: "post",
-        //     headers: {
-        //         Authorization: "Client-ID e6aa071d345d18f"
-        //     },
-        //     body: formdata
-        // }).then(data => data.json()).then(data => {
-        //     console.log(data)
-        // })
-
+        formdata.append("image", selectedFile);
+        formdata.append("image", files)
+        fetch("https://api.imgur.com/3/image", {
+            method: "post",
+            headers: {
+                Authorization: "Client-ID e6aa071d345d18f"
+            },
+            body: formdata
+        }).then(data => data.json()).then(data => {
+            console.log(data)
+        })
     }
 
     render() {
         return (
             <div>
-                <input type="file" multiple onChange={this.onFileChange} accept="image/*" />
+                <input type="file" onChange={this.onFileChange} accept="image/*" />
                 <button onClick={this.onFileUpload}>Upload</button>
             </div >
         )
     }
 }
 export default Imgur
-
-
-
-
-
