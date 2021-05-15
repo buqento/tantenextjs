667997060833919af05dc31b38ee2bccb3d3b69d
buqento <buqento@gmail.com>
2021-05-15 11:18:30 +0700

    add map

--- pages/addnew.js
+++ pages/addnew.js
@@ -6,6 +6,8 @@ import { DtProvinsi } from '../utils/modals/Provinsi'
 import { City } from '../utils/modals/City'
 import withAuth from '../helpers/withAuth';
 import { FiSend } from 'react-icons/fi'
+import ReactMapGl, { FullscreenControl, GeolocateControl, Marker } from 'react-map-gl'
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 function Addnew({ userdata }) {
 
@@ -15,7 +17,6 @@ function Addnew({ userdata }) {
         email: userdata.email,
         photo_url: userdata.photoURL
     }
-    const strToArray = (str) => { return str.trim().split(", ") }
     const initType = {
         Campur: true,
         Putra: false,
@@ -77,6 +78,24 @@ function Addnew({ userdata }) {
         { name: 'rTamu', title: 'R.Tamu' }
     ]
     const facilityTitle = (key) => listFacility.filter(facility => facility.name === key)
+    const strToArray = (str) => { return str.trim().split(", ") }
+
+    // map
+    const accessToken = "pk.eyJ1IjoiYnVxZW50byIsImEiOiJjanJ5a3p4cDkwZXJiNDlvYXMxcnhud3hhIn0.AhQ-vGYSIo6uTBmQD4MCsA"
+    const lat = parseFloat(-6.175428880001885)
+    const long = parseFloat(106.82717876549592)
+    const height = 200
+    const zoom = 10
+    const [viewport, setViewport] = useState({
+        latitude: lat,
+        longitude: long,
+        width: "100%",
+        height: height,
+        zoom: zoom
+    })
+    viewport.width = "100%"
+    viewport.height = height
+    // end map
 
     const [type, setType] = useState(initType)
     const [durations, setDurations] = useState(initDurations);
@@ -90,7 +109,6 @@ function Addnew({ userdata }) {
     const [city, setCity] = useState("Denpasar")
     const [district, setDistrict] = useState("Denpasar Utara")
     const [near, setNear] = useState("")
-    const [lat_lng, setLatlng] = useState("")
     const [contact_phone, setContactPhone] = useState("")
     const [contact_whatsapp, setContactWhatsapp] = useState("")
     const [contact_fb, setContactFb] = useState("")
@@ -189,7 +207,7 @@ function Addnew({ userdata }) {
                                 city: city,
                                 district: district,
                                 near: strToArray(near),
-                                lat_lng: new fire.firestore.GeoPoint(Number(strToArray(lat_lng)[0]), Number(strToArray(lat_lng)[1]))
+                                lat_lng: new fire.firestore.GeoPoint(viewport.latitude, viewport.longitude)
                             },
                             category: "Kost",
                             type: arrType,
@@ -222,7 +240,6 @@ function Addnew({ userdata }) {
                     setCity("")
                     setDistrict("")
                     setNear("")
-                    setLatlng("")
                     setContactPhone("")
                     setContactWhatsapp("")
                     setContactFb("")
@@ -350,13 +367,14 @@ function Addnew({ userdata }) {
 
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Nama Kost</label>
-                <input className="border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none" id="name" type="text" placeholder="Seroja Kostel Jambon" value={name} onChange={(e) => setName(e.target.value)} />
+                <input className="border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none" id="name" type="text" placeholder="Nama Kost" value={name} onChange={(e) => setName(e.target.value)} />
+                <div className="small my-1 text-current">Contoh: <span className="font-bold text-green-500">Kost Exclusive Graha Chempaka</span></div>
             </div>
 
             <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Judul</label>
                 <input className="border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none" id="title" type="text" placeholder="Judul" value={title} onChange={(e) => setTitle(e.target.value)} />
-                <div className="small my-1 text-green-500">Contoh: <span className="font-bold">Seroja Kostel Jambon Kost Murah Sewa Harian Mingguan Bulanan Trihanggo Gamping Sleman Daerah Istimewa Yogyakarta</span></div>
+                <div className="small my-1 text-current">Contoh: <span className="font-bold text-green-500">Kost Exclusive Graha Chempaka Badung Renon Denpasar Selatan Denpasar Bali</span></div>
             </div>
 
             <div className="mb-4">
@@ -477,8 +495,32 @@ function Addnew({ userdata }) {
             </div>
 
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm mb-2 font-bold" htmlFor="lat_lng">Latitude, Longitude</label>
-                <input className="border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none" id="lat_lng" type="text" placeholder="-8.669823629718868, 115.20791945067694" value={lat_lng} onChange={(e) => setLatlng(e.target.value)} />
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="district">Peta Lokasi</label>
+                <ReactMapGl
+                    {...viewport}
+                    mapboxApiAccessToken="pk.eyJ1IjoiYnVxZW50byIsImEiOiJjanJ5a3p4cDkwZXJiNDlvYXMxcnhud3hhIn0.AhQ-vGYSIo6uTBmQD4MCsA"
+                    onViewportChange={viewport => { setViewport(viewport) }}
+                    mapStyle="mapbox://styles/buqento/ckg4bb6cc2hrr19k84gzrs97j"
+                >
+                    <div className="ml-2 mt-2" style={{ width: '29px' }}>
+                        <FullscreenControl label="Perbesar Peta" />
+                    </div>
+                    <div className="ml-2 mt-2" style={{ width: '29px' }}>
+                        <GeolocateControl
+                            positionOptions={{ enableHighAccuracy: true }}
+                            trackUserLocation={true}
+                            label="Lokasi Anda"
+                        />
+                    </div>
+                    <Marker
+                        latitude={viewport.latitude}
+                        longitude={viewport.longitude}
+                        offsetLeft={-18}
+                        offsetTop={-25}
+                    >
+                        <FaMapMarkerAlt size={30} className="text-danger" />
+                    </Marker>
+                </ReactMapGl>
             </div>
 
             <div className="mb-4">
