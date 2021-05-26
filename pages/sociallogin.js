42b83ef71cafb3a98e9eb698716f07fb70081679
buqento <buqento@gmail.com>
2021-05-27 04:34:03 +0700

    debug social login

---
+++ pages/sociallogin.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Link from 'next/link'
+import { auth, firebase } from '../configurations/auth'
+import { Container } from 'react-bootstrap'
+import { FcGoogle } from 'react-icons/fc'
+import router from 'next/router'
+import SocialLogin from 'react-social-login'
+import SocialButton from '../components/SocialButton'
+
+class Sociallogin extends React.Component {
+  handleSignIn = (user) => {
+    console.log(user);
+  }
+  handleSocialLoginFailure = (err) => {
+    console.error(err)
+  }
+  render() {
+    const { children, triggerLogin, ...props } = this.props
+    return (
+      <Container className="text-center my-5">
+        <div className="text-4xl">Login</div>
+
+        <SocialButton
+          provider="google"
+          appId="788082975831-u359tstdmbm4g6b8evnkafvige96cbv9.apps.googleusercontent.com"
+          onLoginSuccess={this.handleSignIn}
+          onLoginFailure={this.handleSocialLoginFailure}
+        >
+          <button>Login with Google</button>
+
+        </SocialButton>
+        {/* <div className="my-5 p-2 text-center border cursor-pointer" onClick={this.handleSignIn}><FcGoogle className="inline mb-1 mr-2" />Login dengan Google</div> */}
+        <div>Kami tidak akan membagikan detail pribadi Anda dengan siapa pun. Jika Anda login, Anda menerima Syarat dan Ketentuan serta <Link href="policy"><span className="underline cursor-pointer">Kebijakan Privasi</span></Link></div>
+        <div className="mt-5 underline"><Link href="/">Kembali ke Beranda</Link></div>
+      </Container>
+    )
+  }
+}
+export default Sociallogin
