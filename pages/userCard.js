6ae0147b152e8c75de0c41f4c43a8fc4ecf479e6
buqento <buqento@gmail.com>
2021-05-28 06:41:40 +0700

    debug login

--- pages/userCard.js
+++ pages/userCard.js
@@ -1,18 +1,18 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 export default class UserCard extends Component {
-    static propTypes = {
-        user: PropTypes.shape({
-            _profile: PropTypes.object,
-            _token: PropTypes.object
-        }),
-        logout: PropTypes.func
-    }
+    // static propTypes = {
+    //     user: PropTypes.shape({
+    //         _profile: PropTypes.object,
+    //         _token: PropTypes.object
+    //     }),
+    //     logout: PropTypes.func
+    // }
 
     render() {
-        const { user: { _profile, _token }, logout } = this.props
-        console.warn('_profile', _profile);
+        // const { user: { _profile, _token }, logout } = this.props
+        // console.warn('_profile', _profile);
 
         return (
             <div >
