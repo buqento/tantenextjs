9fd252c66023b36e966e5a9af1133956fd1632cf
buqento <buqento@gmail.com>
2021-05-28 06:25:52 +0700

    debug login

--- pages/sociallogin.js
+++ pages/sociallogin.js
@@ -3,8 +3,10 @@ import Link from 'next/link'
 import { Container } from 'react-bootstrap'
 import router from 'next/router'
 import SocialButton from '../components/SocialButton'
+import UserCard from './userCard'
 
 class Sociallogin extends React.Component {
+
 
   constructor(props) {
     super(props)
@@ -94,17 +96,19 @@ class Sociallogin extends React.Component {
           </SocialButton>
         </div>
 
+        <UserCard user={user} />
+
         {/* <div className="my-5 p-2 text-center border cursor-pointer" onClick={this.handleSignIn}><FcGoogle className="inline mb-1 mr-2" />Login dengan Google</div> */}
 
         <div>Kami tidak akan membagikan detail pribadi Anda dengan siapa pun. Jika Anda login, Anda menerima Syarat dan Ketentuan serta <Link href="policy"><span className="underline cursor-pointer">Kebijakan Privasi</span></Link></div>
         <div className="mt-5 underline"><Link href="/">Kembali ke Beranda</Link></div>
 
-        <Container className="divide-y-2 divide">
+        {/* <Container className="divide-y-2 divide">
           <div className="flex py-3">
-            <div><img src={user._profile.profilePicURL} alt={user._profile.name} width={50} onError={(e) => { e.target.onerror = null; e.target.src = "/static/images/image-not-found.png" }} /></div>
+            <div><img src={user.photoURL} alt={userdata.displayName} width={50} onError={(e) => { e.target.onerror = null; e.target.src = "/static/images/image-not-found.png" }} /></div>
             <div className="ml-2">
               <div>Hallo,</div>
-              <div className="text-lg font-bold">{user._profile.name}</div>
+              <div className="text-lg font-bold">{userdata.displayName}</div>
             </div>
           </div>
           <Link href="iklansaya">
@@ -115,7 +119,7 @@ class Sociallogin extends React.Component {
           <div className="py-3 cursor-pointer" onClick={this.handleLogout}>
             <FiLogOut className="inline mb-1 mr-1" /> Logout
                 </div>
-        </Container>
+        </Container> */}
 
       </Container>
     )
