c6e244a246f2e412a000b8a1eb61fe76c932faf2
buqento <buqento@gmail.com>
2021-05-29 22:18:18 +0700

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,9 @@ import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 const options = {
     providers: [
-        Providers.Facebook({
-            clientId: process.env.NEXT_PUBLIC_REACT_APP_FB_CLIENT_ID,
-            clientSecret: process.env.NEXT_PUBLIC_REACT_APP_FB_CLIENT_SECRET
-        }),
         Providers.Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
         }),
     ],
     callbacks: {
