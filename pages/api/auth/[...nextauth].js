a39e6f2b27e8c83f39a3b113c2254076456f387e
buqento <buqento@gmail.com>
2021-05-29 15:43:21 +0700

    debug session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,16 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-export default NextAuth({
+
+import Providers from 'next-auth/providers'
+
+const options = {
     providers: [
         Providers.Facebook({
             clientId: '3234331779955939',
             clientSecret: '36c5221cdbdfe874b6f13fd59eac1c19'
         })
-    ]
-})
+    ],
+    site: 'https://tantekos.com'
+}
+
+export default (req, res) => NextAuth(req, res, options)
