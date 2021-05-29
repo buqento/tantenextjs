edde8b6639668f4ba6548f95f1134de73838cf7e
buqento <buqento@gmail.com>
2021-05-29 19:31:05 +0700

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,13 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-export default NextAuth({
+
+const options = {
     providers: [
         Providers.Facebook({
             clientId: process.env.NEXT_PUBLIC_REACT_APP_FB_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_REACT_APP_FB_CLIENT_SECRET
         })
     ]
-})
+}
+
+export default (req, res) => NextAuth(req, res, options)
