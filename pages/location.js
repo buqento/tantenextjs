a337a092a1df54c961734d00fc8927b847eee0f2
buqento <buqento@gmail.com>
2021-06-05 11:53:10 +0700

    setup ads

--- pages/location.js
+++ pages/location.js
@@ -105,9 +105,6 @@ class MapView extends React.Component {
                 <AdSense.Google
                     client='ca-pub-1434074630735871'
                     slot='7863233219'
-                    style={{ display: 'block' }}
-                    layout='in-article'
-                    format='fluid'
                 />
                 <Geocoder
                     className="border text-lg mx-3 my-3"
