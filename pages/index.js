db63d19788c5bf4d9cec886bf8fa6acd5bc7692e
buqento <buqento@gmail.com>
2021-06-05 14:27:43 +0700

    setup ads

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,8 @@ class Index extends React.Component {
               <AdSense.Google
                 client='ca-pub-1434074630735871'
                 slot='3178716891'
-                className="h-52 w-full"
+                className="w-full"
+                style={{ height: 350 }}
                 format='auto'
               />
             </div>
@@ -132,7 +133,7 @@ class Index extends React.Component {
               <AdSense.Google
                 client='ca-pub-1434074630735871'
                 slot='7863233219'
-                className="h-56 w-full"
+                className="h-64 w-full"
                 format=''
               />
             </div>
