4988a4f98fb3019233f8ef2162dd48dd14277fd4
buqento <buqento@gmail.com>
2021-06-18 16:23:17 +0700

    add area

--- utils/modals/Area.js
+++ utils/modals/Area.js
@@ -25,7 +25,8 @@ export const DtArea = [
     { province: 'Sumatera Utara', city: 'Medan', district: 'Medan Tembung' },
     { province: 'Sumatera Utara', city: 'Padangsidempuan', district: 'Padangsidimpuan Angkola Julu' },
     { province: 'Sumatera Utara', city: 'Padangsidempuan', district: 'Padangsidimpuan Batunadua' },
-
+    { province: 'Sumatera Utara', city: 'Kota Medan', district: 'Medan Timur' },
+    
     { province: 'Maluku', city: 'Ambon', district: 'Teluk Ambon' },
     { province: 'Maluku', city: 'Ambon', district: 'Baguala' },
     { province: 'Maluku', city: 'Ambon', district: 'Sirimau' },
