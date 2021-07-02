1046540eb827eb5e80645e39dac799d0a5f31387
buqento <buqento@gmail.com>
2021-07-02 07:54:08 +0700

    improve footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 export default function Footer() {
-    return <div className="text-center py-3 border-top">
-        <div className="text-sm text-indigo-700">
-            <Link href="/">Home</Link> &middot; <Link href="/about">About Us</Link> &middot; <Link href="/contact">Contact</Link> &middot; <Link href="/policy">Privacy Policy</Link>
+    return <div style={{ marginBottom: '85px' }}>
+        <div className="xs:hidden text-center py-3 border-top">
+            <div className="text-sm text-indigo-700">
+                <Link href="/">Home</Link> &middot; <Link href="/about">About Us</Link> &middot; <Link href="/contact">Contact</Link> &middot; <Link href="/policy">Privacy Policy</Link>
+            </div>
+            <small className="text-gray-700">&copy; 2019-{new Date().getFullYear()} Tantekos. All Rights Reserved.</small>
         </div>
-        <small className="text-gray-700">&copy; 2019-{new Date().getFullYear()} Tantekos. All Rights Reserved.</small>
     </div>
 }
